5facd42f811bc0afc3e7d93e4f7107b9449eac0f
Michael <mkenton2020@icloud.com>
2021-08-24 17:13:36 -0500

    added closing div tag to Home to resolve error

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,17 @@
 import UserDisplay from "./UserDisplay"
 import NewUserForm from "./NewUserForm"
 
-export default function Home({users, newUserSubmit, selectPlayer}) {
+export default function Home({ users, newUserSubmit, selectPlayer }) {
 
     return (
-
         <div>
             <h1 className="component-header">Home</h1>
             <div className="home">
-              <h2>Who's Playing?</h2>
-              {users.map((user) => <UserDisplay user={user} key={user.id} selectPlayer={selectPlayer} />)}
-              <NewUserForm newUserSubmit={newUserSubmit} />
+                <h2>Who's Playing?</h2>
+                {users.map((user) => <UserDisplay user={user} key={user.id} selectPlayer={selectPlayer} />)}
+                <NewUserForm newUserSubmit={newUserSubmit} />
+            </div>
         </div>
     )
+
 }
